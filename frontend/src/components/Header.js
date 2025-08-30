2ba34b89f2f9b5beb22e2d3acad8c40f68a93166
Chahraoui Idriss <chahraouiidriss@gmail.com>
2025-08-30 18:27:58 +0200

    aa

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Menu, X } from 'lucide-react';
-import logo from '../assets/logo.png';
+import logo from '../assets/logo.png'
 
 const Header = () => {
     const [isOpen, setIsOpen] = useState(false);
